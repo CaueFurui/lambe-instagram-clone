12351fcb36b64e171cd44c7f6a05d7c4f7f63682
CaueFurui <cauefurui@gmail.com>
2020-08-21 20:50:54 -0300

    [LINT]Adding ESlint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es2020: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
