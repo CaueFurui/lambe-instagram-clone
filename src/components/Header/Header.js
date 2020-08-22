39c8d95039ca218fb6699592b2990274fa39d821
CaueFurui <cauefurui@gmail.com>
2020-08-22 15:08:37 -0300

    [HEADER]Creating header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
+import {
+  Text,
+  View,
+  Image
+} from 'react-native'
+import styles from './styles'
+import icon from '../../../assets/imgs/icon.png'
+
+class Header extends Component {
+  render () {
+    return (
+      <View style={styles.container}>
+        <View style={styles.rowContainer}>
+          <Image source={icon} style={styles.image}/>
+          <Text style={styles.title}> Lambe Lambe </Text>
+        </View>
+
+      </View>
+    )
+  }
+}
+
+export default Header
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,7 +1,27 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Platform } from 'react-native'
 
 const styles = StyleSheet.create({
-
+  container: {
+    marginTop: Platform.OS === 'ios' ? 20 : 0,
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#bbb'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  image: {
+    height: 30,
+    width: 30,
+    resizeMode: 'contain'
+  },
+  title: {
+    color: '#000',
+    fontFamily: 'Raleway',
+    height: 30,
+    fontSize: 24
+  }
 })
 
 export default styles
